8e0dc0da3dbd6d2da4f0a11886938a8675189ab0
Marijn Stapert <mf.stapert@gmail.com>
2024-03-15 15:49:36 +0100

    remove redundant config

--- frontend/ui/jest.config.ts
+++ frontend/ui/jest.config.ts
@@ -1,8 +1,6 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  clearMocks: true,
-  coverageProvider: "v8",
 };
 
 export default config;
--- frontend/website/jest.config.ts
+++ frontend/website/jest.config.ts
@@ -1,8 +1,6 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  clearMocks: true,
-  coverageProvider: "v8",
   moduleNameMapper: {
     "@ui/(.*)": "<rootDir>/frontend/ui/$1"
   }
