f7fc40537b217b069739d198c36d903f13423dbb
mstapert <mf.stapert@gmail.com>
2024-03-22 23:21:37 +0100

    move e2e config to seperate folder

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost/cms',
-    supportFile: 'e2e/cms-e2e/support/e2e.ts',
-    specPattern: 'e2e/cms-e2e/tests/**/*.cy.ts',
+    supportFile: 'cms-e2e/support/e2e.ts',
+    specPattern: 'cms-e2e/tests/**/*.cy.ts',
   },
 });
