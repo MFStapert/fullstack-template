34da31abc05fe1e08674292ecc5ced8fe5ae9751
marijn <mf.stapert@gmail.com>
2024-08-15 23:31:52 +0200

    add meets module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { LocationsModule } from './locations/locations.module';
+import { MeetsModule } from './meets/meets.module';
+import { UsersModule } from './users/users.module';
 
-const features = [LocationsModule];
+const features = [MeetsModule, LocationsModule, UsersModule];
 
 @Module({
   imports: [...features, DbModule, ConfigModule.forRoot({ isGlobal: true })],
